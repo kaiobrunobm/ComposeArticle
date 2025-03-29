96a2cc1b34736e2d144524e3aee0e809c83e8297
kaiobrunobm <bmkaiobruno@gmail.com>
2025-03-29 13:26:14 -0300

    Adding a image to the header and adding a article with 2 paragraphs and a title

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    Arrangement.Top
+                    ){
+                    HeaderImage(modifier = Modifier)
+                    ArticleText(
+                        "Jetpack Compose tutorial",
+                        "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                        "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name",
+                        modifier = Modifier
                     )
                 }
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun HeaderImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null,
+        Modifier.fillMaxWidth()
     )
+}
+
+@Composable
+fun ArticleText(articleTitle: String,paragraphOne: String, paragraphTwo: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = articleTitle,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(24.dp)
+        )
+        Text(
+            text = paragraphOne,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = paragraphTwo,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        HeaderImage(modifier = Modifier)
     }
 }
